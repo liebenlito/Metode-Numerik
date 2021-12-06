681aeebdac68c3ef3b4cb2cd089cd7c02156cf07
muhazaliebenlito@gmail.com <muhazaliebenlito@gmail.com>
2021-12-06 18:08:29 +0700

    edit vscode

---
+++ _build/jupyter_execute/notebooks/bvp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Solusi Numerik untuk Masalah Syarat Batas
+# 
+# Bentuk umum
+# 
+# $$
+# y'' = f(x, y, y'), \hspace{1.5em} a \leq x \leq b, \hspace{1.5em} y(a) = \alpha \text{ dan } y(b) = \beta.
+# $$
+# 
+# Berdasarkan linearitas masalahnya 
+
+# In[ ]:
+
+
+
+
